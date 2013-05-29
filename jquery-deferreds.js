1db80b5d4a7f62409f3d0e3114bf5653ba728c5b
Marcus Baer <marcus.baer@greenpocket.de>
2013-05-29 10:43:02 +0200

    deferreds

---
+++ jquery-deferreds.js
@@ -0,0 +1,40 @@
+﻿var MyView = Backbone.View.extend({
+
+	initialize: function () {
+		_.bindAll(this, "render");
+		this.fetchSomeData();
+	},
+
+	fetchSomeData: function () {
+		$.when(this.fetchFoo(), this.fetchBar())
+			.always(function (fooData, barData) {
+				this.render();
+			});
+	},
+
+	fetchFoo: function () {
+		var deferred = new $.Deferred();
+		$.get('/foo', function(fooData){
+			deferred.resolve(fooData);
+		}, function(){
+			deferred.reject([]);
+		});
+		return deferred.promise();
+	},
+
+	fetchBar: function () {
+		var deferred = new $.Deferred();
+		$.get('/bar', function(barData){
+			deferred.resolve(barData);
+		}, function(){
+			deferred.reject([]);
+		});
+		return deferred.promise();
+	},
+
+	render:function () {
+		$(this.el).html('');
+		return this;
+	}
+
+});
